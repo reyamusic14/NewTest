ac73d63cb265a6c2115f0e863f9fa7e0199a42e4
aadya gupta <aesthetical.aadya09@gmail.com>
2025-02-21 13:33:53 +0100

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    STABILITY_API_KEY: sk-HFQ5mF7E1bDIvoIIKo3wdnUE8ZnYPJKcCM4Kpldgw21hye00
-    DALLE_API_KEY: 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
+    STABILITY_API_KEY: string
+    DALLE_API_KEY: string
     MIDJOURNEY_API_KEY: string
   }
 }
